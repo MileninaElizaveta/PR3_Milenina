c25b5d3092757dc3770c0227f49a3b66eda42172
MileninaElizaveta <159778043+MileninaElizaveta@users.noreply.github.com>
2024-09-10 15:41:15 +0300

    Initial commit

---
+++ PR3/MainForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net;
+using PR3.UsersClasses;
+
+
+namespace PR3
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            textBoxEmail.Text = "evmilenium@mail.ru";
+            textBoxName.Text = "Миленина Елизавета";
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxEmail.Text) ||
+            string.IsNullOrWhiteSpace(textBoxName.Text) ||
+            string.IsNullOrWhiteSpace(textBoxSubject.Text) ||
+            string.IsNullOrWhiteSpace(textBoxBody.Text))
+            {
+                MessageBox.Show("Заполните все поля!");
+                return;
+            }
+                
+            string smtp = "smtp.mail.ru";
+            StringPair fromInfo = new StringPair("evmilenium@mail.ru", "Миленина Елизавета Викторовна");
+            string password = "U8SJTv28yDyQtpmCXi5Q";
+
+            StringPair toInfo = new StringPair(textBoxEmail.Text, textBoxName.Text);
+            string subject = textBoxSubject.Text;
+            string body = $"{DateTime.Now} \n" +
+            $"{Dns.GetHostName()} \n" +
+            $"{Dns.GetHostAddresses(Dns.GetHostName()).First()} \n" +
+            $"{textBoxBody.Text}";
+
+            InfoEmailSending info =
+            new InfoEmailSending(smtp, fromInfo, password, toInfo, subject, body);
+            SendingEmail sendingEmail = new SendingEmail(info);
+            sendingEmail.Send();
+
+            MessageBox.Show("Письмо отправлено!");
+            foreach (TextBox textBox in Controls.OfType<TextBox>())
+                textBox.Text = "";
+        }
+    }
+}
---
+++ PR3/UsersClasses/SendingEmail.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace PR3.UsersClasses
+{
+    public class SendingEmail
+    {
+        private InfoEmailSending InFoEmailSending { get; set; }
+
+        public SendingEmail(InfoEmailSending infoEmailSending)
+        {
+            InFoEmailSending = infoEmailSending
+            ?? throw new ArgumentNullException(nameof(infoEmailSending));
+        }
+        public void Send()
+        {
+            try
+            {
+                SmtpClient mySmtpClient = new SmtpClient(InFoEmailSending.SmtpClientAdress);/*SmtpClient(InfoEmailSending.SmtpClientAdress);*/
+
+                mySmtpClient.UseDefaultCredentials = false;
+                mySmtpClient.EnableSsl = true;
+
+                NetworkCredential basicAuthenticationInfo = new
+                NetworkCredential(InFoEmailSending.EmailAdressFrom.EmailAdress, InFoEmailSending.EmailPassword);
+                mySmtpClient.Credentials = basicAuthenticationInfo;
+
+
+                MailAddress from = new MailAddress(
+                InFoEmailSending.EmailAdressFrom.EmailAdress, InFoEmailSending.EmailAdressFrom.Name);
+
+                MailAddress to = new MailAddress(
+                InFoEmailSending.EmailAdressTo.EmailAdress,
+                InFoEmailSending.EmailAdressTo.Name);
+                MailMessage myMail = new MailMessage(from, to);
+
+                MailAddress replyTo =
+                new MailAddress(InFoEmailSending.EmailAdressFrom.EmailAdress);
+                myMail.ReplyToList.Add(replyTo);
+
+                Encoding encoding = Encoding.UTF8;
+
+                myMail.Subject = InFoEmailSending.Subject;
+                myMail.SubjectEncoding = encoding;
+
+                myMail.Body = InFoEmailSending.Body;
+                myMail.BodyEncoding = encoding;
+
+                mySmtpClient.Send(myMail);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
